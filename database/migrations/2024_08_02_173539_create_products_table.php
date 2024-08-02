0de5aa908552fbc8138ecddc71ff7210dd9d9eb7
Barakat <abdallah771363885@gmail.com>
2024-08-02 21:44:48 +0300

    database changes

---
+++ database/migrations/2024_08_02_173539_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('purchase_price', 8, 2);
+            $table->boolean('published')->default(true);
+            $table->string('slug')->unique();
+            $table->boolean('approved')->default(false);
+            $table->foreignId('brand_id')->nullable()->constrained();
+            $table->boolean('cash_on_delivery')->default(false);
+            $table->boolean('free_shipping')->default(false);
+            $table->foreignId('user_id')->constrained();
+            $table->integer('stock')->default(0);
+            $table->string('discount_type', 50)->default('percentage')->nullable();
+            $table->decimal('discount', 8, 2)->default(0);
+            $table->timestamp('discount_start')->nullable();
+            $table->timestamp('discount_end')->nullable();
+            $table->string('external_link')->nullable();
+            $table->decimal('num_of_sales', 8, 2)->default(0);
+            $table->integer('rating')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_08_02_173705_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->foreignId('parent_id')->nullable()->constrained('categories');
+            $table->integer('order')->default(0);
+            $table->boolean('published')->default(true);
+            $table->string('image')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('color')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
